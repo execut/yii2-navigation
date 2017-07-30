94a297bc7d0f44a59366b327cbf2635bf75ddf91
eXeCUT <sexecut@gmail.com>
2017-07-30 21:52:59 +0300

    fixed not found message

--- page/NotFound.php
+++ page/NotFound.php
@@ -5,6 +5,7 @@
 namespace execut\navigation\page;
 
 
+use Codeception\PHPUnit\ResultPrinter\HTML;
 use execut\navigation\Page;
 
 class NotFound extends Page
@@ -12,7 +13,12 @@ class NotFound extends Page
     public function __construct($config = [])
     {
         parent::__construct($config);
-        $this->setName('Page not found');
-        $this->setText('Sorry page does\'t exist');
+        $this->setName('Not found');
+        $this->setText(<<<HTML
+<p>Page is not found.</p>
+<p>The above error occurred while the Web server was processing your request.</p>
+<p>Please contact us if you think this is a server error. Thank you.</p>
+HTML
+);
     }
 }
