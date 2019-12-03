5552fde43438fc1259f9fc2f05dfab5801da73e2
eXeCUT <sexecut@gmail.com>
2019-12-03 15:47:44 +0300

    fixed tests

--- TestCase.php
+++ TestCase.php
@@ -5,8 +5,35 @@
 namespace execut\navigation;
 
 
-class TestCase extends \yii\codeception\TestCase
+use yii\di\Container;
+use yii\helpers\ArrayHelper;
+
+class TestCase extends \PHPUnit\Framework\TestCase
 {
+    protected function mockWebApplication($config = [], $appClass = '\yii\web\Application')
+    {
+        new $appClass(ArrayHelper::merge([
+            'id' => 'testapp',
+            'basePath' => __DIR__,
+            'vendorPath' => dirname(__DIR__) . '/vendor',
+            'components' => [
+                'request' => [
+                    'cookieValidationKey' => 'wefJDF8sfdsfSDefwqdxj9oq',
+                    'scriptFile' => __DIR__ .'/index.php',
+                    'scriptUrl' => '/index.php',
+                ],
+            ]
+        ], $config));
+    }
+
+    /**
+     * Destroys application in Yii::$app by setting it to null.
+     */
+    protected function destroyApplication()
+    {
+        \Yii::$app = null;
+        \Yii::$container = new Container();
+    }
     public $appConfig = [
         'id' => 'navigation-test',
         'basePath' => __DIR__,
