c636f71f8797ec4458eb87ec82a4a366df6c114d
eXeCUT notebook <sexecut@gmail.com>
2020-07-19 19:39:12 +0300

    debug travis

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -10,7 +10,11 @@ defined('YII_DEBUG') or define('YII_DEBUG', true);
 
 defined('YII_ENV') or define('YII_ENV', 'test');
 
-require_once(__DIR__ . '/../../../autoload.php');
+$autoloadPath = __DIR__ . '/../../../autoload.php';
+if (!file_exists($autoloadPath)) {
+    $autoloadPath = __DIR__ . '/../vendor/autoload.php';//For travis
+}
+require_once($autoloadPath);
 
 require_once(__DIR__ . '/../../../yiisoft/yii2/Yii.php');
 
